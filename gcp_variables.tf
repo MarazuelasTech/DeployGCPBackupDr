d53839d221e1c5be9678c6a788c01c17c23061d6
Willy Rodriguez-Jurado <willy@rjurado.com>
2025-11-14 08:59:09 +0100

    A

--- gcp_variables.tf
+++ gcp_variables.tf
@@ -10,3 +10,25 @@ variable "deploy_region" {
   description = "The GCP region to deploy resources in."
   type        = string
 }
+
+variable "deploy_zone" {
+  description = "The GCP zone to deploy the VM in."
+  type        = string
+}
+
+variable "machine_type" {
+  description = "The machine type for the VM instance."
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "network_name" {
+  description = "The name of the VPC network."
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet."
+  type        = string
+}
+
